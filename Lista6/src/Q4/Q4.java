00114c265652888299921d66b71d208993f72844
LuisE <lecmendoza@id.uff.br>
2014-10-21 18:01:08 -0200

    Questões 2,3 e 4 Ressolvidas

--- Lista6/src/Q4/Q4.java
+++ Lista6/src/Q4/Q4.java
@@ -2,24 +2,25 @@ package Q4;
 
 public class Q4 {
 
-    private static void binSearch(int num, int[] array, int min, int max) {
-        int[] aux = array;
-        if (max > min) {
+    private static int binSearch(int num, int[] array, int min, int max) {
+        if (max <= min) {
+            return max;
+        } else {
             if (num < array[max]) {
-                int auxTam = (min + max)/2;
-                if(num > array[auxTam]){
+                int auxTam = (min + max) / 2;
+                if (num > array[auxTam]) {
                     min = auxTam;
-                }else{
+                } else {
                     max = auxTam;
                 }
-                binSearch(num,array,min,max);
+                return binSearch(num, array, min, max);
             }
+            return max;
         }
-        System.out.println(max);
     }
 
     public static void main(String[] args) {
-        int[] n = {1,2,3,4,5,6,7};
-        binSearch(4,n,0,n.length-1);
+        int[] n = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Posição : " + binSearch(4, n, 0, n.length - 1));
     }
 }
